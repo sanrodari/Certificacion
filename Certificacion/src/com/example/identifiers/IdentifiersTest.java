77fede5b0c85da0dc83a34a14f4ac5af6e68fad5
sanrodari <sanrodari@gmail.com>
2011-07-20 15:06:04 -0500

    Im saying hello to Git, Yeah!!

---
+++ Certificacion/src/com/example/identifiers/IdentifiersTest.java
@@ -0,0 +1,26 @@
+package com.example.identifiers;
+
+public class IdentifiersTest {
+
+	// NO compila porque el super() debe ser la primera instruccion
+	// public IdentifiersTest() {
+	// System.out.println("d");
+	// super();
+	// }
+
+	public static void main(String[] args) {
+		// Object o:m = null; no compila
+		// Object o#m = null; no compila
+		double o = 0.0;
+
+		boolean postcondicion = true;
+		// Se cumple con la condicion
+		assert postcondicion : "No se cumple la pos";
+
+		// Se reservan para q no se usen
+		// goto const
+
+		System.out.println("");
+	}
+
+}
