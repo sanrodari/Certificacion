ff8ddb6271912a222a7b2f365d7e7a8e7a7e203d
sanrodari <sanrodari@gmail.com>
2011-07-26 22:14:48 -0500

    Implementing interfaces, Legal Return Types (covariant), Constructors

---
+++ Certificacion/src/com/example/chapter2/constructors/Constructors.java
@@ -0,0 +1,15 @@
+package com.example.chapter2.constructors;
+
+/**
+ * - Todas las clases deben tener constructores, incluso hasta las abstractas. 
+ * - Si no esta escrito en la entonces el constructor es implicito. 
+ * - The default constructor has the same access modifier as the class. 
+ * - The default constructor has no arguments. 
+ * - The default constructor includes a no-arg call to the super constructor (super()).
+ * 
+ * @author sanrodari
+ * 
+ */
+public class Constructors {
+
+}
